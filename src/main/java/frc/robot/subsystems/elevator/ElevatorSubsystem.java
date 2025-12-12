6e9d669634bc0d0ae0bae028163956d376415eae
Anish Gupta <83379728+neur0n-7@users.noreply.github.com>
2025-12-11 16:40:55 -0800

    add logging with mech 2d to elevator subsystem (double check this works)

--- src/main/java/frc/robot/subsystems/elevator/ElevatorSubsystem.java
+++ src/main/java/frc/robot/subsystems/elevator/ElevatorSubsystem.java
@@ -12,8 +12,18 @@ public class ElevatorSubsystem extends SubsystemBase {
 
     private final NeoMotor motor;
 
+    private double dist = 0.0;
+
     private ElevatorStates state = ElevatorStates.LOWEST;
     private double setpoint = 0.0; // in meters
+
+    private Mechanism2d mechanism = new Mechanism2d(3, 3);
+    private LoggedMechanismRoot2d root =  mechanism.getRoot("elevator", 1, 0);
+    private LoggedMechanismLigament2d elevatorMech = root.append(
+        new LoggedMechanismLigament2d("elevator", ElevatorConstants.elevatorBaseHeight, 90);
+    )
+
+    private MecahnismRoot2d elevatorRoot = elevator
 
     private ProfiledPIDController profiled = new ProfiledPIDController(
             ElevatorConstants.kP, ElevatorConstants.kI, ElevatorConstants.kD,
@@ -43,16 +53,23 @@ public class ElevatorSubsystem extends SubsystemBase {
     }
 
     private double getHeight(){
-        // TODO: finish ts
-        System.out.print(motor.getPosition());
-        return 0.0;
+        // TODO: double check this
+        return motor.getPosition() * ElevatorConstants.gearing; 
     }
 
     public boolean atSetpoint(){ return profiled.atGoal(); }
 
 
-    @Override
-    public void periodic() {
+    // used in sim only
+    private void updateElevatorDist(double dist){
+        this.dist = dist;
+        mechanism.setLength(ElevatorConstants.elevatorBaseHeight + getHeight());
+    }
+
+    /*
+     * commands shared by periodic() and simulationPeriodic()
+    */
+    public void execUpdates(){
         double currentPosition = getHeight();
 
         double ffVolts = feedforward.calculate(
@@ -73,31 +90,17 @@ public class ElevatorSubsystem extends SubsystemBase {
         SmartDashboard.putNumber("Elevator/FF Output (V)", ffVolts);
         SmartDashboard.putNumber("Elevator/Total Voltage", totalVolts);
         SmartDashboard.putString("Elevator/State", state.toString());
+    }
 
 
+    @Override
+    public void periodic() {
+        execUpdates();
     }
 
     @Override
     public void simulationPeriodic() {
-        double currentPosition = getHeight();
-
-        double ffVolts = feedforward.calculate(
-            profiled.getSetpoint().velocity,
-            0
-        );
-        double pidOutput = profiled.calculate(currentPosition);
-
-        double totalVolts = MathUtil.clamp(ffVolts + pidOutput, -12, 12);
-
-        motor.setVoltage(totalVolts);
-
-        // Logging
-        SmartDashboard.putNumber("Elevator/Setpoint", setpoint);
-        SmartDashboard.putNumber("Elevator/CurrentPos", currentPosition);
-        SmartDashboard.putNumber("Elevator/Velocity", profiled.getSetpoint().velocity);
-        SmartDashboard.putNumber("Elevator/PID Output (V)", pidOutput);
-        SmartDashboard.putNumber("Elevator/FF Output (V)", ffVolts);
-        SmartDashboard.putNumber("Elevator/Total Voltage", totalVolts);
-        SmartDashboard.putString("Elevator/State", state.toString());
+        execUpdates();
+        updateElevatorDist();
     }
 }
